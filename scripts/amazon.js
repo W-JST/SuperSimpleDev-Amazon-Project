dabc9b91cb05b86c4f219c13f433b0dcd264cd60
W-JST <workspacejst@gmail.com>
2025-11-11 23:37:28 +0530

    Updated the add to cart button so that the cart quantity gets added accordingly

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,3 @@
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -26,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -67,12 +66,15 @@ products.forEach((product) => {
         }
       });
 
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      let itemQuantity = Number(quantitySelector.value);
+
       if(matchingItem) {
-        matchingItem.quantity++;
+        matchingItem.quantity += itemQuantity;
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: itemQuantity
         });
       }
 
