3eb9bcdfad4090dccb852078ab08537e70d7a213
W-JST <workspacejst@gmail.com>
2025-11-20 19:05:04 +0530

    Until Exercise 17i

--- data/products.js
+++ data/products.js
@@ -11,7 +11,7 @@ export function getProduct (productId) {
   return matchingProduct; 
 }
 
-class Product {
+export class Product {
 
   id;
   image;
@@ -40,7 +40,7 @@ class Product {
   }
 }
 
-class Clothing extends Product {
+export class Clothing extends Product {
 
   sizeChartLink;
 
@@ -54,6 +54,32 @@ class Clothing extends Product {
     return `
       <a href="${this.sizeChartLink}" target="_blank">
         Size chart
+      </a>
+    `;
+  }
+
+}
+
+export class Appliance extends Product{
+
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    
+    return `
+      <a href="${this.instructionsLink}" target="_blank">
+        Instructions
+      </a>
+      <a href="${this.warrantyLink}" target="_blank">
+        Warranty
       </a>
     `;
   }
@@ -151,7 +177,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink:"images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -336,7 +365,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink:"images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -701,7 +733,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink:"images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -785,6 +820,8 @@ export const products = [
 
   if(productDetails.type === 'clothing') {
     return new Clothing(productDetails);
+  } else if (productDetails.type === 'appliance') {
+    return new Appliance(productDetails);
   }
   return new Product(productDetails);
 });
