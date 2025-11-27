f9a8b466bdfee6df328a09494954791c52190ca9
JS-Tharun <workspacejst@gmail.com>
2025-11-27 11:33:48 +0530

    load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary', () => {
 
@@ -7,6 +8,12 @@ describe('test suite: renderOrderSummary', () => {
   const productName1 = 'Black and Gray Athletic Cotton Socks - 6 Pairs';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
   const productName2 = 'Intermediate Size Basketball';
+
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
 
   beforeEach(() => {
 
